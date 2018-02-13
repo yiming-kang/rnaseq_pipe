89c802380010732328d1201ed796cd25a8ba00a9
yiming-kang <yiming.kang@wustl.edu>
2018-02-13 11:28:26 -0600

    updated building design table

--- tools/prepare_samples.py
+++ tools/prepare_samples.py
@@ -35,11 +35,13 @@ def build_sample_summary(samples, metadata, df_cols, qc_cols, group, conditions)
 	for md in metadata:
 		if not os.path.exists(md):
 			sys.exit('ERROR: %s does not exist.' % md)
-		print '... Adding samples in %s' % md
+		print '... Adding samples from %s' % md
 		df = populate_sample_summary(df, md, df_cols, qc_cols, conditions)
 	## force group number if multiple metadata are given
 	if group is not None:
 		df['GROUP'] = group
+	## sort dataframe
+	df = df.sort_values(['SAMPLE'])
 	return df
 
 
@@ -106,43 +108,61 @@ def get_exisiting_samples(df, check_cols):
 	return samples
 
 
-def save_dataframe(filepath, df, df_cols=None):
-	"""
-	Save dataframe of quality assessment.
-	"""
-	df = df.sort_values(['SAMPLE'])
-	if not filepath.endswith('.xlsx'):
-		filepath += '.xlsx'
-	if df_cols is None:
-		df.to_excel(filepath, index=False)
-	else:
-		df.to_excel(filepath, columns=df_cols, index=False)
-
-
 def build_design_table(summary_df, cmp_cols, wt=None):
 	"""
 	Automatically build design table that has basic comparison groups.
 	"""
+	print '... Building design table'
 	## intialize design dataframe
-	design_df = summary_df[['GENOTYPE', 'REPLICATE']].copy()
+	design_df = summary_df[['GENOTYPE', 'REPLICATE', 'SAMPLE'] + cmp_cols].copy()
+	cmp_cols = ['GENOTYPE'] + cmp_cols
 	## create a dict for col: vals
 	col_dict = {}
 	for col in cmp_cols:
 		col_dict[col] = pd.unique(summary_df[col])
 	## iterate thru columns to compare
 	for col in cmp_cols:
-		if len(col_dict[col]) > 1:
-			## get value combination of other column descriptors
-			other_cols = copy.copy(cmp_cols)
-			other_cols.remove(col)
-			other_vals = [col_dict[ocol] for ocol in other_cols]
-			other_vals_combo = make_list_product(other_vals)
+		if len(col_dict[col]) < 2: 
+			## nothing to contrast with
+			continue	
+		## get value combination of other column descriptors
+		other_cols = copy.copy(cmp_cols)
+		other_cols.remove(col)
+		n_other_cols = len(other_cols)
+		other_vals = [col_dict[ocol] for ocol in other_cols]
+		other_vals_combos = make_list_product(other_vals)
+		## iterate thru with other column value fixed
+		for vcombo in other_vals_combos:
+			## get rows matching the column value combo
+			df2 = summary_df[sum([summary_df[other_cols[k]] == vcombo[k] \
+								for k in range(n_other_cols)]) == n_other_cols]
+			vcombo_name = '|'.join([':'.join([other_cols[k], vcombo[k]]) \
+									for k in range(n_other_cols)])
 			## assess diff column types
 			if col == 'GENOTYPE' and wt is not None:
-				col ## TODO
-			else: ## TODO: extend the function for other columns
+				vals = col_dict[col].tolist()
+				vals.remove(wt)
+				for val in vals:
+					## concatenate new column and set flag
+					new_col = ''.join(['[',vcombo_name,']',col,':',wt,'~',val])
+					design_df = pd.concat([design_df, pd.Series(['']*design_df.shape[0], name=new_col)], axis=1)
+					design_df.loc[df2.index[df2[col] == wt], new_col] = '1'
+					design_df.loc[df2.index[df2[col] == val], new_col] = '1'
+			else: 
+				## TODO: extend the function for other columns
 				pass
+	## sort dataframe
+	design_df = design_df.sort_values(cmp_cols + ['SAMPLE'])
 	return design_df
+
+
+def save_dataframe(filepath, df, df_cols=None, freeze_p=None):
+	"""
+	Save dataframe of quality assessment.
+	"""
+	if not filepath.endswith('.xlsx'):
+		filepath += '.xlsx'
+	df.to_excel(filepath, index=False, columns=df_cols, freeze_panes=freeze_p)
 
 
 def main(argv):
@@ -167,13 +187,14 @@ def main(argv):
 	## define pre-alignment criteria to use
 	qc_columns = ['BA', 'TAPESTATION', 'SPIKEIN_READS', 'SAMPLE_READS']
 	## build sample summary sheet
-	summary_df = build_sample_summary(parsed.samples, metadata, df_columns, qc_columns, parsed.group_num, conditions)
-	# save_dataframe(parsed.samples, summary_df, df_columns)
+	summary_df = build_sample_summary(parsed.samples, metadata, df_columns,
+									qc_columns, parsed.group_num, conditions)
+	# save_dataframe(parsed.samples, summary_df, df_cols=df_columns)
 
 	## build design table
-	comparison_columns = ['GENOTYPE'] + conditions
-	design_df = build_design_table(df[df['GROUP']==parsed.group_num], comparison_columns, parsed.wildtype)
-	# save_dataframe(parsed.design_table, design_df)
+	design_df = build_design_table(summary_df[summary_df['GROUP']==parsed.group_num],
+									conditions, parsed.wildtype)
+	save_dataframe(parsed.design_table, design_df, freeze_p=(1,len(conditions)+3))
 
 
 if __name__ == '__main__':
