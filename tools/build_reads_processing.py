b64647e08468e54b99985f2e8e1d5fe9cc2d70fa
yiming-kang <yiming.kang@wustl.edu>
2018-04-12 11:54:20 -0500

    used CDS reads via HTSeq for count matrix

--- tools/build_reads_processing.py
+++ tools/build_reads_processing.py
@@ -12,8 +12,6 @@ def parse_args(argv):
 						help='Genome index file built for the aligner, e.g. *.nix for novoalign.')
 	parser.add_argument('-r', '--reference_gtf', required=True,
 						help='Annotation reference file in GTF/GFF3 format.')
-	parser.add_argument('-l', '--gene_list', required=True,
-						help='Gene list.')
 	parser.add_argument('-g', '--group_num', required=True,
 						help='Experiment group number.')
 	parser.add_argument('-o', '--output_filepath', required=True,
@@ -43,7 +41,7 @@ def build_header(samples_filepath, group, email=None):
 	job += '#SBATCH -D ./\n#SBATCH -o log/readsproc_%A_%a.out\n#SBATCH -e log/readsproc_%A_%a.err\n#SBATCH -J readsproc\n'
 	if email is not None:
 		job += '#SBATCH --mail-type=END,FAIL\n#SBATCH --mail-user=%s\n' % email
-	job += '\nml novoalign/3.07.00\nml samtools/1.6\nml stringtie/1.3.3b\nml R/3.2.1\n'
+	job += '\nml novoalign/3.07.00\nml samtools/1.6\nml stringtie/1.3.3b\nml htseq/0.9.1\nml R/3.2.1\n'
 	job += 'read data1 data2 < <( sed -n ${SLURM_ARRAY_TASK_ID}p %s )\nset -e\n\n' % lookup_filepath
 	return job
 
@@ -64,16 +62,21 @@ def build_alignment(genome_index):
 	return job
 
 
-def build_expression_quantification(reference_gtf, gene_list):
+def build_expression_quantification(reference_gtf, expr_tool='htseq'):
 	"""
-	Build job scripts for quantifying gene expression levels using StringTie
+	Build job scripts for quantifying gene expression levels using StringTie or HTSeq
 	"""
 	job = 'if [[ ! -z ${data1} && ! -z ${data2} ]]; then\n'
-	job += 'if [ ! -f expression/stringtie/${data1}/stringtie_out.gtf  ]; then\n' \
-		+ '\trm -rf expression/stringtie/${data1} || :\n' \
-		+ '\tmkdir -p expression/stringtie/${data1}\n' 
-	job += '\tstringtie -p ${SLURM_CPUS_PER_TASK} alignment/novoalign/${data1}/aligned_reads_sorted.bam -G %s -e -o expression/stringtie/${data1}/stringtie_out.gtf -A expression/stringtie/${data1}/gene_abundances.tab\n' % reference_gtf
-	# job += '\tpython tools/stringtie2fpkm.py expression/stringtie/${data1}/gene_abundances.tab %s > expression/stringtie_fpkm/${data1}.expr\n' % gene_list
+	if expr_tool == 'htseq':
+		job += 'if [ ! -f expression/htseq/${data1}/cds_count.tsv ]; then\n' \
+			+ '\trm -rf expression/htseq/${data1} || :\n' \
+			+ '\tmkdir -p expression/htseq/${data1}\n' 
+		job += '\thtseq-count -f bam -s no -t CDS alignment/novoalign/${data1}/aligned_reads_sorted.bam %s > expression/htseq/${data1}/cds_count.tsv\n' % reference_gtf
+	elif expr_tool == 'stringtie':		
+		job += 'if [ ! -f expression/stringtie/${data1}/stringtie_out.gtf  ]; then\n' \
+			+ '\trm -rf expression/stringtie/${data1} || :\n' \
+			+ '\tmkdir -p expression/stringtie/${data1}\n' 
+		job += '\tstringtie -p ${SLURM_CPUS_PER_TASK} alignment/novoalign/${data1}/aligned_reads_sorted.bam -G %s -e -o expression/stringtie/${data1}/stringtie_out.gtf -A expression/stringtie/${data1}/gene_abundances.tab\n' % reference_gtf
 	job += 'fi\n'
 	job += 'fi\n\n'
 	return job
@@ -92,15 +95,17 @@ def build_count_normalization(group, read_length):
 	return job
 
 
-def prepare_lookup_file(samples, group):
+def prepare_lookup_file(samples, group, expr_tool='htseq'):
 	"""
 	Prepare lookup file for sbatch runs
 	"""
+	expr_tool_dict = {'htseq': 'cds_count.tsv',
+						'stringtie': 'stringtie_out.gtf'}
 	## create lookup data for each sample
 	lookup_readsproc, lookup_expr = '', ''
 	for i,row in samples.iterrows(): 
 		sample = row['GENOTYPE'] +'-'+ row['SAMPLE']
-		expr_file = 'expression/stringtie/'+ sample +'/stringtie_out.gtf'
+		expr_file = '/'.join(['expression', expr_tool, sample, expr_tool_dict[expr_tool]])
 		lookup_readsproc += '%s\t%s\n' % (sample, row['FILE'])
 		lookup_expr += '%s\t%s\n' % (sample, expr_file)
 	## create laste line 
@@ -131,8 +136,6 @@ def main(argv):
 		sys.exit('ERROR: %s does not exist.' % parsed.genome_index)
 	if not os.path.exists(parsed.reference_gtf):
 		sys.exit('ERROR: %s does not exist.' % parsed.reference_gtf)
-	if not os.path.exists(parsed.gene_list):
-		sys.exit('ERROR: %s does not exist.' % parsed.gene_list)
 	if not os.path.exists(os.path.dirname(parsed.output_filepath)):
 		sys.exit('ERROR: %s does not exist.' % os.path.dirname(parsed.output_filepath))
 	
@@ -141,7 +144,7 @@ def main(argv):
 	print '... Building scripts for alignment'
 	jobs += build_alignment(parsed.genome_index)
 	print '... Building scripts for gene expression quantification'
-	jobs += build_expression_quantification(parsed.reference_gtf, parsed.gene_list)
+	jobs += build_expression_quantification(parsed.reference_gtf)
 	print '... Building scripts for count normalization'
 	# jobs += build_count_normalization(parsed.group_num, parsed.read_length)
 	write_file(jobs, parsed.output_filepath)
---
+++ tools/prep_htseq_counts.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+import sys
+import argparse
+import os.path
+import pandas as pd
+import numpy as np
+
+
+def parse_args(argv):
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--input_lookup', required=True,
+						help='Lookup table of sample and the corresponding HTSeq count.')
+	parser.add_argument('-o', '--output_count_file', required=True,
+						help='Output count matrix.')
+	parser.add_argument('-g', '--gene_list', required=True,
+						help='Gene list. This gene list should be a subset or the same set of annotated genes in GTF/GFF.')
+	return parser.parse_args(argv[1:])
+
+
+def parse_lookup(file):
+	lookup = {}
+	data = np.loadtxt(file, dtype=str)
+	for i in range(data.shape[0]):
+		if not os.path.exists(data[i,1]):
+			sys.exit('ERROR: %s does not exist.\n... Aborted preparing count matrix.' % data[i,1])
+		lookup[data[i,0]] = data[i,1]
+	return lookup
+
+
+def create_count_matrix(lookup, gene_list):
+	lookup = parse_lookup(lookup)
+	gene_list = np.loadtxt(gene_list, dtype=str)
+	count_mtx = gene_list.reshape(-1,1)
+	header = ['gene_id']
+	for sample, file in lookup.iteritems():
+		print '... working on %s' % sample
+		count = np.loadtxt(file, dtype=str)
+		indx = np.where([gene_list[i]==count[:,0] for i in range(len(gene_list))])[1]
+		count_mtx = np.hstack((count_mtx, count[indx,1].reshape(-1,1)))
+		header.append(sample)
+	count_mtx = np.vstack((header, count_mtx))
+	return count_mtx
+
+
+def main(argv):
+	parsed = parse_args(argv)
+	if not os.path.exists(parsed.input_lookup):
+		sys.exit('ERROR: %s does not exist.' % parsed.input_lookup)
+	if not os.path.exists(parsed.gene_list):
+		sys.exit('ERROR: %s does not exist.' % parsed.gene_list)
+	
+	count_matrix = create_count_matrix(parsed.input_lookup, parsed.gene_list)
+	np.savetxt(parsed.output_count_file, count_matrix, delimiter=',', fmt='%s')
+	
+
+if __name__ == '__main__':
+	main(sys.argv)
